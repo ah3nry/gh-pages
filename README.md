3d044f64e60f61722af26e228f4444bc78c5838d
AJHenry <andrew.henry@zoo.ox.ac.uk>
2016-02-20 21:30:34 +0000

    adding terminal window

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# gh-pages
-Source code for GH pages
+ah3nry personal pages
+This project is built using HTML5 Boilerplate](https://html5boilerplate.com)
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+var initShell = function() {
+    jQuery(function($, undefined) {
+        var width = $(window).innerWidth() - 100,
+            height = $(window).innerHeight() - 100;
+        $('#cursor').hide();
+        $('#term').terminal(function(command, term) {
+            if (command !== '') {
+                var result = window.eval(command);
+                if (result != undefined) {
+                    term.echo(String(result));
+                }
+            }
+        }, {
+            greetings: '&nbsp;',
+            name: 'js_demo',
+            height: height,
+            width: width,
+            prompt: '> '
+        });
+    });
+};
+
+setTimeout(initShell, 6000);
